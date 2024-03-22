f712928beb368beecea0c2bc58426ba491fc3f56
AlexGriffithsUOL <80417774+AlexGriffithsUOL@users.noreply.github.com>
2024-03-22 17:14:14 +0000

    Fix deploy

--- core/settings/__init__.py
+++ core/settings/__init__.py
@@ -5,6 +5,9 @@ import os
 if ('ENV' in os.environ) == False:
     load_dotenv()
 
+print('env', os.environ['ENV'])
+print('env-2', ENV)
+
 IS_HEROKU = 'IS_DYNO' in os.environ
 
 if IS_HEROKU and ENV == ENV_STAGING:

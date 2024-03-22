fc5b92f4f3f5cd3f6c48f8b2dc715246fa5027c7
AlexGriffithsUOL <80417774+AlexGriffithsUOL@users.noreply.github.com>
2024-03-22 17:04:29 +0000

    Changed webpages, made them more modular

--- base/urls.py
+++ base/urls.py
@@ -6,6 +6,9 @@ from django.conf.urls.static import static
 from . import views
 urlpatterns = [
     path("", view=views.index.as_view(), name="index"),
+    path("pricing", view=views.pricing.as_view(), name="pricing"),
+    path("about", view=views.about.as_view(), name="about"),
+    path("contact", view=views.contact.as_view(), name="contact"),
 ]
 
 if settings.DEBUG:

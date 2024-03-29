d88cfc31ad3c83dd6b50f87a0b7309d62995a41e
AlexGriffithsUOL <80417774+AlexGriffithsUOL@users.noreply.github.com>
2024-03-29 20:07:11 +0000

    Added in user models to test

--- base/models.py
+++ base/models.py
@@ -1,3 +1,46 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser
 # Create your models here.
+class pyrl_company_domain(models.Model):
+    company_name = models.CharField(max_length=100)
+    company_description = models.CharField(max_length=100)
+    company_address = models.CharField(max_length=100)
+    company_phone = models.CharField(max_length=100)
+    company_email = models.CharField(max_length=100)
+    company_website = models.CharField(max_length=100)
+    company_logo = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.company_name}'
+
+
+class pyrl_user(AbstractBaseUser):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=10)
+    company = models.ForeignKey(pyrl_company_domain, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+# class Reporter(models.Model):
+#     first_name = models.CharField(max_length=30)
+#     last_name = models.CharField(max_length=30)
+#     email = models.EmailField()
+
+#     def __str__(self):
+#         return f"{self.first_name} {self.last_name}"
+
+
+# class Article(models.Model):
+#     headline = models.CharField(max_length=100)
+#     pub_date = models.DateField()
+#     reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return self.headline
+
+#     class Meta:
+#         ordering = ["headline"]
--- base/urls.py
+++ base/urls.py
@@ -8,7 +8,7 @@ from . import views
 app_name = 'base'
 
 urlpatterns = [
-    path("", view=views.index.as_view(), name="index"),
+    path("home", view=views.index.as_view(), name="index"),
     path("pricing", view=views.pricing.as_view(), name="pricing"),
     path("about", view=views.about.as_view(), name="about"),
     path("contact", view=views.contact.as_view(), name="contact"),

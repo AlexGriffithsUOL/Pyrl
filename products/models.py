d96e8788a5a567b654a2e660a0dc4c8dfa5ef216
AlexGriffithsUOL <80417774+AlexGriffithsUOL@users.noreply.github.com>
2024-04-06 12:03:47 +0100

    Added in product models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+from django.forms import TextInput
 # Create your models here.
+
+class pyrl_product(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    product_name = models.CharField(max_length=100)
+    product_description = models.TextField()
+    product_price = models.DecimalField(max_digits=5, decimal_places=2)
+    product_image = models.ImageField(upload_to='product_images')
+    product_category = models.CharField(max_length=100)
+
+
+
+    def __str__(self):
+        return f'{self.product_name}'

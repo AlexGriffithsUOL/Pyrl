b56f6b81d42a3a49593a751631f4e67702250ece
AlexGriffithsUOL <80417774+AlexGriffithsUOL@users.noreply.github.com>
2024-07-13 21:40:46 +0100

    Change: removing static folder so it can be gitignored :P

--- static/js/delete_element.js
+++
@@ -1,5 +0,0 @@
-function deleteItem(id) {
-    var element = document.getElementById(id);
-    element.parentNode.removeChild(element);
-    console.log('Deleted');
-}
--- static/js/display_by_id.js
+++
@@ -1,8 +0,0 @@
-function display_by_id(id, string) {
-    var x = document.getElementById(id);
-    if (x.classList.contains(string)) {
-        x.classList.remove(string);
-    } else {
-        x.classList.add(string);
-    }
-}

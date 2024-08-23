159aa425213563a4dcbd5b4644b6bc06d86c780a
AlexGriffithsUOL <80417774+AlexGriffithsUOL@users.noreply.github.com>
2024-08-23 13:06:15 +0100

    fix: debug status should be determined by env vars

--- core/settings/staging.py
+++ core/settings/staging.py
@@ -7,7 +7,7 @@ ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split(', ')
 INSTALLED_APPS.append('whitenoise.runserver_nostatic')
 MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
 
-DEBUG = False
+DEBUG = True
 
 NAME = os.environ['NAME']
 DIVIDER = os.environ['DIVIDER']
